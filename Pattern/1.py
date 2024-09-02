64c8e957c398d4d6c8c5bc2ec98d360b20ea031d
Parshuram Singh <parshuramsingh433@gmail.com>
2024-09-02 19:17:49 +0530

    second commit

--- Pattern/1.py
+++ Pattern/1.py
@@ -10,4 +10,8 @@ Printing for this pattern
 n = int(input("enter the No of rows you want : "))
 
 for i in range(n):
-    print("*" * n)
+    for j in range(n):
+        print("*",end=" ")
+    print(" ")
+        
+         
