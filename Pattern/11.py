3d2d726c9ca0c9920e79e2bbf142467ce0c348a3
Parshuram Singh <parshuramsingh433@gmail.com>
2024-09-02 22:49:51 +0530

    added file

---
+++ Pattern/11.py
@@ -0,0 +1,13 @@
+n = 3
+
+for i in range(0,n):
+    if i%2 == 0:
+        start = 1
+    else:
+        start = 0
+
+    for j in range(0,i+1):
+        print(start,end=" ")
+        start = 1 - start
+    print(" ")
+    
