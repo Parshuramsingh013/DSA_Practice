7d329c670330157ee7c92d57b32fb0fb844ebb59
Parshuram Singh <parshuramsingh433@gmail.com>
2024-09-28 10:13:33 +0530

    introduction

---
+++ Recursion/introduction.py
@@ -0,0 +1,18 @@
+# def ajit():
+#     print("1")
+#     ajit()
+
+# ajit()
+
+
+count = 0
+
+def ajit():
+    global count
+    if count == 4:
+        return
+    print(count)
+    count += 1
+    ajit()
+
+ajit()
