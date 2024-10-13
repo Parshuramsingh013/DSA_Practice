3985ff76ee2b1606e0ff2558c1b509a32416b0a9
Parshuram Singh <parshuramsingh433@gmail.com>
2024-10-13 13:39:14 +0530

    IsArraySorted or Not

---
+++ Array/checkArraySorted.py
@@ -0,0 +1,11 @@
+arr = [1,2,4,5,6,8,9,52,3]
+
+is_sorted = False
+for i in range(1,len(arr)):
+    if arr[i] >= arr [i-1]:
+        is_sorted = True
+    else:
+        is_sorted = False
+    
+print(is_sorted)
+
