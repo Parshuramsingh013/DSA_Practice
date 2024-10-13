95d9c4a51f7e94d197f73065580c2b21d3477052
Parshuram Singh <parshuramsingh433@gmail.com>
2024-10-13 13:26:40 +0530

    Updated

--- Array/SecondLargestElemnt.py
+++ Array/SecondLargestElemnt.py
@@ -1,18 +1,20 @@
-# arr = [5,6,9,9,8,1,3,7]
-
-# largest = arr[0]
-
-# for i in range(len(arr)):
-#     if arr[i] > largest:
-#         largest = arr[i]
-
-# secondLargest = -1
-# for i in range(len(arr)):
-#     if arr[i]> secondLargest and arr[i]!= largest:
-#         secondLargest = arr[i]
-
-# print(secondLargest)
 arr = [5,6,9,9,8,1,3,7]
+
+# First Way
+largest = arr[0]
+
+for i in range(len(arr)):
+    if arr[i] > largest:
+        largest = arr[i]
+
+secondLargest = -1
+for i in range(len(arr)):
+    if arr[i]> secondLargest and arr[i]!= largest:
+        secondLargest = arr[i]
+
+print(secondLargest)
+
+# Second way
 largest = 1
 secondLargets = -1
 
@@ -20,7 +22,6 @@ for i in range(len(arr)):
     if arr[i] > largest:
         secondLargets = largest
         largest = arr[i]
-        
     elif arr[i] > secondLargets and arr[i] != largest:
         secondLargets = arr[i]
 
