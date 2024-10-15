ae7d892956eeba0ece02c8f250b6f9268c50b48f
Parshuram Singh <parshuramsingh433@gmail.com>
2024-10-15 10:10:52 +0530

    update left rotate with D place

--- Array/move_0_end.py
+++ Array/move_0_end.py
@@ -1,15 +1,16 @@
 arr = [1,2,0,2,3,0,0,5,0,8]
 n = len (arr)
 
-temp = []
-temp1 = []
+j = -1
+
 for i in range(n):
-    if (arr[i] != 0):
-        temp.append(arr[i])
-    else:
-        temp1.append(arr[i])
+    if arr[i] == 0:
+        j = i
+        break
+for i in range(j+1,n):
+    if arr[i] != 0:
+        arr[i], arr[j] = arr[j], arr[i]
+        j += 1
 
-temp.extend(temp1)
-print(temp)
-
+print(arr)
 
