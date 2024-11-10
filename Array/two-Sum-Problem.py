4db2b9a03732e420cc4d23dc6c9fcad7cffce5a2
Parshuram Singh <parshuramsingh433@gmail.com>
2024-11-10 17:54:09 +0530

    updated

---
+++ Array/two-Sum-Problem.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return[i, j]
+        return []
+    
+nums = [1,5,6,9,7,2,4,3]
+target = 11
+target1 = 16
+target2 = 15
+
+s1 = Solution()
+print(s1.twoSum(nums,target))
+print(s1.twoSum(nums,target1))
+print(s1.twoSum(nums,target2))
+    
